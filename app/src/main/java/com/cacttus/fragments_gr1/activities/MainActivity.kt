56a0bf0ac3b85ed667da5d22580b606c10c1f177
Muala Kabashi <keds30955@KEDS.kv>
2024-12-09 12:05:55 +0100

    add bottom navigation with different fragments

--- app/src/main/java/com/cacttus/fragments_gr1/activities/MainActivity.kt
+++ app/src/main/java/com/cacttus/fragments_gr1/activities/MainActivity.kt
@@ -3,18 +3,33 @@ package com.cacttus.fragments_gr1.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.cacttus.fragments_gr1.R
 import com.cacttus.fragments_gr1.databinding.ActivityMainBinding
+import com.cacttus.fragments_gr1.fragments.FirstFragment
+import com.cacttus.fragments_gr1.fragments.SecondFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        var firstFragment = FirstFragment()
+        var secondFragment = SecondFragment()
+        binding.btnFirstFragment.setOnClickListener {
+            setCurrentFragment(firstFragment)
+        }
+
+        binding.btnSecondFragment.setOnClickListener {
+            setCurrentFragment(secondFragment)
+        }
     }
 
-
     fun setCurrentFragment(fragment: Fragment) {
-
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment)
+        }.commit()
     }
 }
 
