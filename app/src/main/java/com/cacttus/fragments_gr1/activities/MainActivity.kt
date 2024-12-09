a4b6d0058c62fa70c0edd524766bc059e3a16498
Muala Kabashi <mualaakabashi@gmail.com>
2024-12-09 11:00:25 +0100

    add fragments

---
+++ app/src/main/java/com/cacttus/fragments_gr1/activities/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.cacttus.fragments_gr1.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.cacttus.fragments_gr1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    fun setCurrentFragment(fragment: Fragment) {
+
+    }
+}
+
