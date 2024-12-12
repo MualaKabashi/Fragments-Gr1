4ee84ae211be9d7304785e571212fd3366ca48fd
Muala Kabashi <mualaakabashi@gmail.com>
2024-12-12 11:06:55 +0100

    add profile details fragment

---
+++ app/src/main/java/com/cacttus/fragments_gr1/fragments/ProfileDetailsFragment.kt
@@ -0,0 +1,17 @@
+package com.cacttus.fragments_gr1.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProfileDetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
