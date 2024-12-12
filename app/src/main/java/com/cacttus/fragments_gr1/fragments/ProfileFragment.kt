a7ba0706d47920aa7135776050abce43861caf37
Muala Kabashi <keds30955@KEDS.kv>
2024-12-12 11:06:55 +0100

    add profile details fragment

--- app/src/main/java/com/cacttus/fragments_gr1/fragments/ProfileFragment.kt
+++ app/src/main/java/com/cacttus/fragments_gr1/fragments/ProfileFragment.kt
@@ -6,13 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cacttus.fragments_gr1.R
+import com.cacttus.fragments_gr1.databinding.FragmentProfileBinding
 
-class ProfileFragment :Fragment() {
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSend.setOnClickListener {
+            var value = binding.etText.text.toString()
+            var bundle = Bundle()
+            bundle.putString("data", value)
+
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            addToBackStack(null)
+            commit()
+        }
     }
 }
